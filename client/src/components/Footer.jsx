1509685860e91f39754cc7dc0c0e5d126d61263a
Ronald Paek <ronaldpaek@gmail.com>
2023-01-22 13:59:43 -0800

    create footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-full flex items-center justify-center py-8">
+        <h2 className="text-2xl">Built by chingu-42-gecko-23</h2>
+    </div>
+  )
+}
+
+export default Footer
