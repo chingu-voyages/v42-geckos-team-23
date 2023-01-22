43dbddc262d23588405f6807906ff72b77f5eb8f
Ronald Paek <ronaldpaek@gmail.com>
2023-01-22 13:41:20 -0800

    create arrow button

---
+++ client/src/components/ArrowButton.jsx
@@ -0,0 +1,9 @@
+import { FaGreaterThan } from "react-icons/fa"
+
+const ArrowButton = () => {
+  return (
+    <button className="btn btn-primary btn-circle"><FaGreaterThan /></button>
+  )
+}
+
+export default ArrowButton
