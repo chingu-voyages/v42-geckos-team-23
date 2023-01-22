f8634910cc7664b8b7b285a4d473cb184623a4e7
Ronald Paek <ronaldpaek@gmail.com>
2023-01-22 14:01:50 -0800

    added padding for section

--- client/src/components/Restaurants.jsx
+++ client/src/components/Restaurants.jsx
@@ -3,7 +3,7 @@ import Card from "./Card"
 
 const Restaurants = () => {
   return (
-    <div className="grid place-content-center space-y-4">
+    <div className="grid place-content-center space-y-4 py-8">
         <h2 className="text-5xl text-center lg:text-left lg:text-7xl">Restaurants</h2>
         <div className="mx-auto container grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
             {businesses.map(business => (
