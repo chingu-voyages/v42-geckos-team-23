416b616f86759cc07080acd3732d921e2ea1b724
Ronald Paek <ronaldpaek@gmail.com>
2023-01-24 18:38:29 -0800

    added red bgcolor to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-full flex items-center justify-center py-8">
+    <div className="w-full flex items-center justify-center py-8 bg-red-50">
         <h2 className="text-2xl">Built by chingu-42-gecko-23</h2>
     </div>
   )
