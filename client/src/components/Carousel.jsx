c3ebd7d85f1283e8f924a9c3d28b1a7526d83daf
Ronald Paek <ronaldpaek@gmail.com>
2023-01-31 14:07:29 -0800

    Fix pagination bullets

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -15,31 +15,29 @@ SwiperCore.use([EffectFade, Pagination, Navigation, Autoplay])
 
 function Carousel() {
     return (
-        <>
-            <Swiper
-                spaceBetween={30}
-                loop={true}
-                effect={'fade'}
-                centeredSlides={true}
-                pagination={{
-                    clickable: true,
-                }}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                navigation={true}
-                className="mySwiper"
-            >
-                {slide_images.map((img, i) => {
-                    return (
-                        <SwiperSlide key={i}>
-                            <img src={img} alt="" />
-                        </SwiperSlide>
-                    )
-                })}
-            </Swiper>
-        </>
+        <Swiper
+            spaceBetween={30}
+            loop={true}
+            effect={'fade'}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+            navigation={true}
+            className="mySwiper"
+        >
+            {slide_images.map((img, i) => {
+                return (
+                    <SwiperSlide key={i}>
+                        <img src={img} alt="" />
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     )
 }
 
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,9 +3,15 @@ import Carousel from './Carousel'
 
 function Hero() {
   return (
-    <div>
-        <Carousel />
-    </div>
+      <div className="relative">
+          <h1 className="absolute top-[10%] right-[10%] z-10 text-[70px] leading-tight text-white font-bold font-nunito text-shadow">
+              Take your pets
+              <br />
+              wherever you go!
+          </h1>
+
+          <Carousel />
+      </div>
   )
 }
 
