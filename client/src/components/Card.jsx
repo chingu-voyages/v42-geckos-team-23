9cbee9dca9357882fb45b60f039fbf21a5174f53
Ronald Paek <ronaldpaek@gmail.com>
2023-01-30 22:17:36 -0800

    Fix card rating

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,10 @@
-import { Rating } from 'react-simple-star-rating'
 
+import StarRating from './StarRating'
 import Address from './Address'
 import ArrowButton from './ArrowButton'
 
 const Card = ({ id, name, location, rating, image_url, price }) => {
+    console.log(rating)
     return (
         <div className="card w-80 bg-base-100 shadow-xl sm:w-96">
             <figure className="relative h-72">
@@ -14,23 +15,16 @@ const Card = ({ id, name, location, rating, image_url, price }) => {
                 />
             </figure>
             <div className="card-body flex-row justify-between">
-                <div className="w-3/5">
+                <div className="w-4/5">
                     <h2 className="card-title">{name}</h2>
                     <Address location={location} row={2} />
                 </div>
-                <div className="flex w-2/5 flex-col items-end">
+                <div className="flex w-1/5 flex-col items-end">
                     {/* TODO: if price is null, take white space. */}
 
                     {price && <p className='flex-grow-0'>{price}</p>}
 
-                    <Rating
-                        initialValue={rating}
-                        readOnly={true}
-                        size={16}
-                        fillColor={'#FC0706'}
-                        allowFraction
-                        SVGstyle={{ display: 'inline-block' }}
-                    />
+                    <StarRating rating={rating} />
                 </div>
 
                 <div className="card-actions absolute top-4  right-4 justify-end">
