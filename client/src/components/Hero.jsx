2d51fd4dc6f41133af5431c5e016cc3e7559030f
Ronald Paek <ronaldpaek@gmail.com>
2023-02-02 15:42:04 -0800

    fix hero button

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,21 +1,27 @@
-import React, { useRef } from 'react'
+import React, { useRef, useContext } from 'react'
+
+import { Context } from '../../Context'
 import ArrowButton from './ArrowButton'
 import Carousel from './Carousel'
 
 function Hero() {
     const arrowButtonRef = useRef()
+    const ctx = useContext(Context)
 
     return (
         <div className="relative z-0">
-            <h1 className="text-shadow absolute top-[10%] left-0 right-0 text-center md:text-left md:left-auto  z-10 font-nunito text-[50px] font-bold leading-tight text-white md:right-[5%] md:text-[70px]">
+            <h1 className="text-shadow absolute top-[10%] left-0 right-0 z-10 text-center font-nunito  text-[50px] font-bold leading-tight text-white md:left-auto md:right-[5%] md:text-left md:text-[70px]">
                 Take your pets
                 <br />
                 wherever you go!
             </h1>
-            <ArrowButton
-                ref={arrowButtonRef}
-                className="absolute top-[5%] left-[5%] z-10"
-            />
+            {ctx.isSearchBtnClicked && (
+                <ArrowButton
+                    ref={arrowButtonRef}
+                    className="absolute top-[5%] left-[5%] z-10"
+                />
+            )}
+
             <Carousel />
         </div>
     )
