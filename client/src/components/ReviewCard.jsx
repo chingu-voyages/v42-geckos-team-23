10221e61ed37a0420367f211c6aa7f999bd8ac0d
jiyunnoh <ianjy1127@gmail.com>
2023-02-02 18:02:27 -0800

    Add the default user profile image

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import defaultUserProfile from '../assets/goPup_dog.png'
 
 const ReviewCard = ({ user, text, time_created }) => {
     const date = time_created.slice(0, 10)
+    user.image_url === null && (user.image_url = defaultUserProfile)
 
     return (
         <section className='font-nunito w-1/3 pr-6'>
             <div className='flex items-center'>
                 <img className='w-12 h-12 rounded-full'
-                    // TODO: set default image
                     src={user.image_url}
                     alt={`${user.name}'s profile picture on Yelp`} />
                 <div className='ml-5'>
