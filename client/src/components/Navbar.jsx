21bc74e4f95352eb47f6a74631f99ed40273c026
Aaronokwok <aaronkwok101@gmail.com>
2023-02-03 14:57:59 -0800

    modify navbar for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import mobileLogo from '../assets/goPup_mobile-logo.png';
 import logo from '../assets/goPup_logo.png';
 import searchIcon from '../assets/search-icon.svg';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Spin as Hamburger} from 'hamburger-react';
 
 const Navbar = () => {
     const [categoryName, setCategoryName] = useState('')
@@ -21,20 +22,21 @@ const Navbar = () => {
 
     return (
         <div className="bg-red-50">
-            <div className=" flex flex-col px-8 py-8 items-center justify-between md:flex-row">
+            <div className="flex px-3 py-3 lg:px-8 lg:py-8 items-center justify-between md:flex-row">
                 <img
-                    className="w-[175px] pb-8 sm:hidden"
+                    className="w-[55px] sm:hidden"
                     src={mobileLogo}
                     alt="logo"
+                    onClick={goHome}
                 />
                 <img
-                    className="sm:w-[150px] hidden sm:block sm:pb-4 hover:cursor-pointer"
+                    className="sm:w-[150px] hidden sm:block hover:cursor-pointer"
                     src={logo}
                     alt="logo"
                     onClick={goHome}
                 />
 
-                <form className="flex flex-col items-center gap-4 md:flex-row md:gap-1 lg:gap-4">
+                <form className="hidden lg:flex flex-col items-center gap-4 md:ml-10 md:flex-row md:gap-1 lg:gap-4">
                     <ZipCodeInput variant="navbar" />
                     <CategoryInputs variant="navbar" setCategoryName={setCategoryName} />
                     <Button variant="navbar" categoryName={categoryName}>
@@ -42,9 +44,13 @@ const Navbar = () => {
                             className="pr-1 md:pr-0"
                             src={searchIcon}
                         />
-                        <span className="md:hidden">Search</span>
                     </Button>
                 </form>
+
+                {/* mobile */}
+                <div className="lg:hidden">
+                    <Hamburger rounded size={28} direction="right"/>
+                </div>
             </div>
         </div>
     )
