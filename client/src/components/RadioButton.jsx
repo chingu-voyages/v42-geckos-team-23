3dd5fe0cce7b59b12924932173625e07ba2e4f00
Ronald Paek <ronaldpaek@gmail.com>
2023-02-04 14:51:23 -0800

    Create new RadioButton component

---
+++ client/src/components/RadioButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function RadioButton({ label, category, value, onChange}) {
+  return (
+    <label>
+        <input className="mr-1" type="radio" data-category={category} data-category-name={label} checked={value} onChange={onChange} />
+        {label}
+    </label>
+  )
+}
+
+export default RadioButton
