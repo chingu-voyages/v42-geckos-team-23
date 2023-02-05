765bb1d00c048ef514503289e5b2077a468456a1
Ronald Paek <ronaldpaek@gmail.com>
2023-02-04 19:52:52 -0800

    Changed some css for for photos

--- client/src/components/BusinessPhtos.jsx
+++ client/src/components/BusinessPhtos.jsx
@@ -15,10 +15,10 @@ const BusinessPhotos = ({ id }) => {
     return (
         <section className='m-10 font-nunito'>
             <h1 className="mb-7 text-3xl font-bold">Photos</h1>
-            <div className="my-10 grid xl:grid-cols-3">
+            <div className="my-10 flex flex-col gap-4 xl:gap-0 sm:grid sm:grid-cols-3">
                 {details.photos?.map((photo) => (
-                    <div key={photo} className=" w-96 shadow-xl">
-                        <figure className="relative h-72">
+                    <div key={photo} className="flex-1 shadow-xl md:max-w-[384px] ">
+                        <figure className="h-72">
                             <img
                                 className="h-full w-full object-cover"
                                 src={photo}
