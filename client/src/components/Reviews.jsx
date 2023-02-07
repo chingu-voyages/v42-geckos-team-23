fe1a351ac9d5e548569953ea72de85552b105faa
Ronald Paek <ronaldpaek@gmail.com>
2023-02-07 03:44:18 -0800

    Add target and rel attribute on anchor tag

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -23,20 +23,21 @@ const Reviews = ({ id }) => {
     }, [])
 
     return (
-        <section className="mx-10 mt-10 mb-20 font-nunito relative">
+        <section className="relative mx-10 mt-10 mb-20 font-nunito">
             <h1 className="mb-7 text-3xl font-bold">Reviews</h1>
-            <div className="mb-6 flex flex-col gap-4 sm:gap-6 sm:grid sm:grid-cols-3">
+            <div className="mb-6 flex flex-col gap-4 sm:grid sm:grid-cols-3 sm:gap-6">
                 {reviews}
             </div>
-            {
-                (3 === reviews.length) &&
-                    <a
-                        className="absolute right-0 rounded-full border-2 border-solid border-red-500 px-7 py-2 text-red-500 active:bg-red-200 active:text-white hover:cursor-pointer"
-                        href={reviews[0]?.props.url}
-                    >
-                        More Reviews on Yelp
-                    </a>
-            }
+            {3 === reviews.length && (
+                <a
+                    className="absolute right-0 rounded-full border-2 border-solid border-red-500 px-7 py-2 text-red-500 hover:cursor-pointer active:bg-red-200 active:text-white"
+                    href={reviews[0]?.props.url}
+                    target="_blank"
+                    rel="noopener"
+                >
+                    More Reviews on Yelp
+                </a>
+            )}
         </section>
     )
 }
