482d300c5dcc0d993f722ef6874f0cf509e07545
jiyunnoh <ianjy1127@gmail.com>
2023-02-08 17:35:07 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://gopup.onrender.com',
         changeOrigin: true,
       }
     }
