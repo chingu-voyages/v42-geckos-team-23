91c9a2d7e1238055fb9c869e0e27a3a6083b0fc9
jiyunnoh <ianjy1127@gmail.com>
2023-02-08 17:44:57 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       }
     }
