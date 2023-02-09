c55fed6d84356a85cfaad4321a91d62bd6bbe1e6
Ronald Paek <ronaldpaek@gmail.com>
2023-02-08 21:27:54 -0800

    Update functions to be async

--- client/src/api/YelpAPI.jsx
+++ client/src/api/YelpAPI.jsx
@@ -1,14 +1,14 @@
-const getBusinessesFromYelpApi = (location, category) => {
+const getBusinessesFromYelpApi = async (location, category) => {
     return fetch(`/api/yelp?location=${location}&term=dog+friendly&categories=${category}`)
         .then((res) => res.json())
 }
 
-const getDetailsByIdFromYelpApi = (id) => {
+const getDetailsByIdFromYelpApi = async (id) => {
     return fetch(`/api/yelp/${id}`)
         .then((res) => res.json())
 }
 
-const getReviewsFromYelpApi = (id) => {
+const getReviewsFromYelpApi = async (id) => {
     return fetch(`/api/yelp/${id}/reviews`)
         .then((res => res.json()))
 }
