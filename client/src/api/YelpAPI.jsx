795290c6a365145f838b06f89c669833bbed2791
Ronald Paek <ronaldpaek@gmail.com>
2023-02-08 20:19:34 -0800

    Add some console.log

--- client/src/api/YelpAPI.jsx
+++ client/src/api/YelpAPI.jsx
@@ -1,6 +1,12 @@
 const getBusinessesFromYelpApi = (location, category) => {
-    return fetch(`/api/yelp?location=${location}&term=dog+friendly&categories=${category}`)
-        .then((res) => res.json())
+    return fetch(
+        `/api/yelp?location=${location}&term=dog+friendly&categories=${category}`
+    ).then((res) => {
+        if (res.ok) {
+            return res.json()
+        }
+        return Promise.reject(res)
+    })
 }
 
 const getDetailsByIdFromYelpApi = (id) => {
