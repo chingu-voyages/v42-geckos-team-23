4e00eba077a4f8810f72660a31151aee14e34cb5
jiyunnoh <ianjy1127@gmail.com>
2023-02-09 21:09:28 -0800

    Edit vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
+  },
+  base: './',
+  define: {
+    'process.env.NODE_ENV': 'production'
   }
 })
--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-// const path = require('path');
+const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -9,12 +9,12 @@ const app = express();
 
 require('dotenv').config();
 
-// if (process.env.NODE_ENV === 'production') {
-//   //*Set static folder up in production
-//   app.use(express.static('../client/dist'));
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('../client/dist'));
 
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html')));
-// }
+  app.get('/home', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html')));
+}
 
 // Have Node serve the files for our built React app
 // app.use(express.static(path.resolve(__dirname, '../client/dist')));
