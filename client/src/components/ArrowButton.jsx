f6d9ac327f5c57ebb57bc005d05e0340696f17ef
Ronald Paek <ronaldpaek@gmail.com>
2023-02-10 13:06:43 -0800

    Add hover effect to whole card

--- client/src/components/ArrowButton.jsx
+++ client/src/components/ArrowButton.jsx
@@ -1,22 +1,19 @@
 import { useNavigate } from 'react-router-dom'
-import { FaGreaterThan, FaLessThan } from 'react-icons/fa'
+import { FaLessThan } from 'react-icons/fa'
 
-const ArrowButton = ({ id, direction = 'prev', className = '' }) => {
+const ArrowButton = ({ id, className = '' }) => {
     const navigate = useNavigate()
 
-    const clickHandler = (e) => {
-        e.preventDefault()
-
-        { direction === 'next' && navigate(`details/${id}`) }
-        { direction === 'prev' && navigate(-1) }
-
+    const clickHandler = () => {
+        navigate(-1)
     }
+
     const baseStyles = 'btn-primary btn-circle btn'
     const arrowButtonClassName = `${baseStyles} ${className}`
 
     return (
         <button className={arrowButtonClassName} onClick={clickHandler}>
-            {direction === 'prev' ? <FaLessThan /> : <FaGreaterThan />}
+            <FaLessThan />
         </button>
     )
 }
