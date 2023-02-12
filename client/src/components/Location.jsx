61e0b18d5ee74f26c2406725ea3efd10c75b2e3e
Ronald Paek <ronaldpaek@gmail.com>
2023-02-12 02:39:31 -0800

    Add use my location button

---
+++ client/src/components/Location.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { MdOutlineAddLocationAlt } from 'react-icons/md';
+
+const Location = ({ size = 30, className = '', onClick }) => {
+    return (
+        <button className={className} type="button" onClick={onClick}>
+            <MdOutlineAddLocationAlt className="icon z-[1] ml-1" size={size} fill="#EF4444" />
+            <span className="description">
+                Use My Location
+            </span>
+        </button>
+    )
+}
+
+export default Location
